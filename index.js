c3927de1b2b0628ccb7b3805878c94ca96fc1ee5
Ali Akkas <aliakkas006@gmail.com>
2021-10-27 15:53:01 +0600

    server side added

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const { query } = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muank.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// server connected with database
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("online_shop");
+        const productCollection = database.collection("products");
+
+        // GET products api
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Ema john node server is running!');
+});
+
+app.listen(port, () => {
+    console.log(`This app is listen from port: ${port}`);
+});
